b058e0d7f1ac2a17fa743d0554e587f1ea761275
future <18063085192@163.com>
2019-04-17 22:46:17 +0800

    1.添加eslin

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # vue-jquery-pagination
+```
 将jquery.pagination.js分页插件改为vue组件
+```
+
+## 安装yarn
+```
+npm install -g yarn
+```
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn run serve
+```
+
+### Compiles and minifies for production
+```
+yarn run build
+```
+
+### Run your tests
+```
+yarn run test
+```
+
+### Lints and fixes files
+```
+yarn run lint
+
+vue-cli3 选择自定义 可以设置eslin，默认也有eslin，需要自定义设置
+参照：https://www.jianshu.com/p/5e13bc2eb97c
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 记忆
+```
+1.父组件向子组件传参使用props即可传参
+    a.方式
+      props: {
+          msg: String
+        }
+    b.方式
+      props: [
+          'msg'
+        ]  
+2.父组件动态改变子组件的内容，可将更改父组件的标签值，即传值时使用父组件的data即可
+    并通过：v-bind:msg赋值
+3.子组件向父组件传参
+    子组件调用父组件，在父组件方法中修改父组件的参数
+4.父组件调用子组件方法
+    子组件方法：parentHandleclick
+    父组件写法：
+        先给使用组件的标签上ref标识
+        通过使用this.$refs.标识.方法名
+     如：
+         <Page @changeP="changeMsg" ref="myChild" v-bind:msg="myMsg"/>
+         this.$refs.myChild.parentHandleclick("我是爸爸");
+5.子组件调用父组件方法
+    使用this.$emit与自定义事件配合完成，如：
+        子组件Page调用：
+            this.$emit('changeP');
+        父组件：
+            <Page @changeP="changeMsg" v-bind:msg="myMsg"/>
+        即可调用父组件的方法changeMsg
+        
+6.子组件与父组件交互对于input有个简写的方法，可以参照官方：在组件上使用 v-model
+
+```
+
+### ES6写法
+```
+1.{...s,...t}合并数据，已最后一个为准
+```
--- src/components/common/Pageination.vue
+++ src/components/common/Pageination.vue
@@ -10,53 +10,53 @@
 </template>
 
 <script>
-    export default {
-        name: 'Pageination',
-        props: {
-            msg: String
-        },
-        data: function () {
-            return {
-                defaults: {
-                    totalData: 0, //数据总条数
-                    showData: 0, //每页显示的条数
-                    pageCount: 9, //总页数,默认为9
-                    current: 1, //当前第几页
-                    prevCls: 'prev', //上一页class
-                    nextCls: 'next', //下一页class
-                    prevContent: '<', //上一页内容
-                    nextContent: '>', //下一页内容
-                    activeCls: 'active', //当前页选中状态
-                    coping: false, //首页和尾页
-                    isHide: false, //当前页数为0页或者1页时不显示分页
-                    homePage: '', //首页节点内容
-                    endPage: '', //尾页节点内容
-                    keepShowPN: false, //是否一直显示上一页下一页
-                    mode: 'unfixed', //分页模式，unfixed：不固定页码数量，fixed：固定页码数量
-                    count: 4, //mode为unfixed时显示当前选中页前后页数，mode为fixed显示页码总数
-                    jump: false, //跳转到指定页数
-                    jumpIptCls: 'jump-ipt', //文本框内容
-                    jumpBtnCls: 'jump-btn', //跳转按钮
-                    jumpBtn: '跳转', //跳转按钮文本
-                    callback: function () {
-                    } //回调
-                }
-            };
-        },
-        created: {
-            this.defaults = {...this.defaults,totalData:20,showData:10,pageCount:2}
-        },
-        methods: {
-            changeF: function () {
-                console.log("子组件单击开始");
-                this.$emit('changeP');
-            },
-            parentHandleclick(e) {
-                console.log(e)
-                this.childMsg = e;
-            }
-        }
+export default {
+  name: 'Pageination',
+  props: {
+    msg: String
+  },
+  data: function () {
+    return {
+      defaults: {
+        totalData: 0, // 数据总条数
+        showData: 0, // 每页显示的条数
+        pageCount: 9, // 总页数,默认为9
+        current: 1, // 当前第几页
+        prevCls: 'prev', // 上一页class
+        nextCls: 'next', // 下一页class
+        prevContent: '<', // 上一页内容
+        nextContent: '>', // 下一页内容
+        activeCls: 'active', // 当前页选中状态
+        coping: false, // 首页和尾页
+        isHide: false, // 当前页数为0页或者1页时不显示分页
+        homePage: '', // 首页节点内容
+        endPage: '', // 尾页节点内容
+        keepShowPN: false, // 是否一直显示上一页下一页
+        mode: 'unfixed', // 分页模式，unfixed：不固定页码数量，fixed：固定页码数量
+        count: 4, // mode为unfixed时显示当前选中页前后页数，mode为fixed显示页码总数
+        jump: false, // 跳转到指定页数
+        jumpIptCls: 'jump-ipt', // 文本框内容
+        jumpBtnCls: 'jump-btn', // 跳转按钮
+        jumpBtn: '跳转', // 跳转按钮文本
+        callback: function () {
+        } // 回调
+      }
     }
+  },
+  created: function () {
+    this.defaults = {...this.defaults, totalData: 20, showData: 10, pageCount: 2}
+  },
+  methods: {
+    changeF: function () {
+      console.log('子组件单击开始')
+      this.$emit('changeP')
+    },
+    parentHandleclick (e) {
+      console.log(e)
+      this.childMsg = e
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
